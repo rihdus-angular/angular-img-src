d6397392df052db8df08904b9b70bd752cf14df9
Sudhir <rihdus.io@gmail.com>
2016-03-12 18:44:41 +0800

    Add demo js and tempate to index.html

--- demo/index.js
+++ demo/index.js
@@ -2,11 +2,11 @@ angular.module('app', ['ImgSrc'])
   .controller('ImageFallbackController', [
     '$scope', '$timeout',
     function ($scope, $timeout) {
-      $scope.img_initial = "images/AngularJs-gray.png";
+      $scope.img_initial = "demo/images/AngularJs-gray.png";
       $scope.img_external = "http://it-kokensha.com/company/wp-content/uploads/2015/06/full_2125152.jpeg.png";
       $scope.img_external_2 = "http://findicons.com/files/icons/2773/pictonic_free/512/angularjs.png";
-      $scope.img_actual = "images/AngularJs.png";
-      $scope.img_loading = "images/AngularJs-loading.png";
+      $scope.img_actual = "demo/images/AngularJs.png";
+      $scope.img_loading = "demo/images/AngularJs-loading.png";
 
       $scope.img = null;
 
