91a70f95709c05f1905ccccb80f03874a293ca42
Sudhir <rihdus.io@gmail.com>
2016-03-12 16:21:04 +0800

    Add ImgSrc module. Add package and bower json.

---
+++ demo/index.js
@@ -0,0 +1,23 @@
+angular.module('app', ['ImgSrc'])
+  .controller('ImageFallbackController', [
+    '$scope', '$timeout',
+    function ($scope, $timeout) {
+      $scope.img_initial = "images/AngularJs-gray.png";
+      $scope.img_external = "http://it-kokensha.com/company/wp-content/uploads/2015/06/full_2125152.jpeg.png";
+      $scope.img_external_2 = "http://findicons.com/files/icons/2773/pictonic_free/512/angularjs.png";
+      $scope.img_actual = "images/AngularJs.png";
+      $scope.img_loading = "images/AngularJs-loading.png";
+
+      $scope.img = null;
+
+      $timeout(function () {
+        console.log('ctrl updating new image..');
+        $scope.img = $scope.img_external+'';
+      }, 2000);
+
+      $scope.switchImage = function () {
+        $scope.img = $scope.newImageSource+'';
+      }
+
+    }])
+;
